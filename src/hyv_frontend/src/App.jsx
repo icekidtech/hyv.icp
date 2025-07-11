ee2deb5d81ab97e6d12d8f5a073b4b661d299ca3
icekidtech <princekelvin7568@gmail.com>
2025-07-11 04:39:19 +0100

    Refactor App component to integrate model upload, search, and grid functionalities

--- src/hyv_frontend/src/App.jsx
+++ src/hyv_frontend/src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react';
-import { hyv_backend } from 'declarations/hyv_backend';
+import React, { useState, useEffect } from "react";
+import ModelUpload from "./components/ModelUpload";
+import ModelGrid from "./components/ModelGrid";
+import ModelSearch from "./components/ModelSearch";
+import { hyv_backend } from "declarations/hyv_backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [models, setModels] = useState([]);
+  const [searchParams, setSearchParams] = useState({});
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    hyv_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+  useEffect(() => {
+    hyv_backend.listModels().then(setModels);
+  }, []);
+
+  async function handleSearch(params) {
+    const results = await hyv_backend.searchModels(params.domain, params.modelType, params.performance);
+    setModels(results);
+  }
+
+  async function handleUpload(id) {
+    const model = await hyv_backend.getModelNFT(id);
+    setModels(models => [...models, model]);
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <ModelUpload onUpload={handleUpload} />
+      <ModelSearch onSearch={handleSearch} />
+      <ModelGrid models={models} />
     </main>
   );
 }
